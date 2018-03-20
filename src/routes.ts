a9f562bb9cb1a7e821857fc66b3a3798f98cddcd
Juan Pablo Flores Galindo <juanfg@outlook.com>
2018-03-19 23:35:56 -0600

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Application } from 'express';
+
+class Routes {
+
+    constructor(app: Application) {
+        this.getRoutes(app);
+    }
+
+    private getRoutes(app: Application): void {
+    }
+}
+
+export default Routes;
